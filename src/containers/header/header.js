081cf5c286ba7540d2e0635fda80d4ece683dc22
alexnas <alexnas2python@gmail.com>
2020-04-20 16:37:19 +0300

    Made makeup for header.

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import './header.css';
 
 const Header = () => {
   return (
-    <div className='row border-bottom'>
-      <div className='pl-200'>
+    <div className='row border-bottom topbar mb-20'>
+      <Link to='/' className='navbar-brand pl-200 logo-wrap'>
         <img
           src='/images/rattan1.jpeg'
           className='logo-image'
           alt='World of Rattan'
         />
         <span className='logo-name'>Rattan</span>
-      </div>
-      <div className='col-md-2'>
-        <div>Home</div>
-        <div>Leasure</div>
-        <div>Garden</div>
-      </div>
-      <div>
-        <div>Меню</div>
-      </div>
+      </Link>
+
+      <ul className='nav'>
+        <li className='nav-item'>
+          <NavLink className='nav-link btn btn-light btn-sm' to='/login'>
+            <i class='fa fa-sign-in' aria-hidden='true'>
+              &nbsp;
+            </i>
+            Sign in
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink className='nav-link  btn btn-light btn-sm' to='/register'>
+            <i class='fa fa-sign-out' aria-hidden='true'>
+              &nbsp;
+            </i>
+            Sign up
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
-
 export default Header;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import Articles from './containers/articles/articles';
 import Article from './containers/article/article';
 import Basket from './containers/basket/basket';
+import Authentication from './containers/authentication/authentication';
 
 export default (
   <Switch>
     <Route exact path='/' component={Articles} />
-    <Route exact path='/categories/:id' component={Articles} />
-    <Route exact path='/articles/:id' component={Article} />
-    <Route exact path='/basket' component={Basket} />
+    <Route path='/categories/:id' component={Articles} />
+    <Route path='/articles/:id' component={Article} />
+    <Route path='/basket' component={Basket} />
+    <Route path='/login' component={Authentication} />
+    <Route path='/register' component={Authentication} />
   </Switch>
 );
